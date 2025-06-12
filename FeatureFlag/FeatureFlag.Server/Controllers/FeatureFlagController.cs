b7cfad2bb29ff078207294880201f156a700176f
Kushal test <kushal.test1998@hotmail.com>
2025-06-13 00:42:20 +0530

    Changes related to feature flag model

---
+++ FeatureFlag/FeatureFlag.Server/Controllers/FeatureFlagController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FeatureFlag.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeatureFlagController : ControllerBase
+    {
+       
+        // GET api/<FeatureFlagController>/5
+        [HttpGet("{projectName}/{key}")]
+        public string Get(string projectName, string key)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] FeatureFlagModel featureFlagModel)
+        {
+        }
+
+        [HttpPut]
+        public void Put([FromBody] FeatureFlagModel featureFlagModel)
+        {
+
+        }
+
+        [HttpDelete]
+        public void Delete([FromBody] FeatureFlagModel featureFlagModel)
+        {
+        }
+    }
+}
