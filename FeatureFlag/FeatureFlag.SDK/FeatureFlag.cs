925147259594df8c9bef2bd04af74efcb09855eb
Kushal test <kushal.test1998@hotmail.com>
2025-07-05 12:33:03 +0530

    Creating new api sample

--- FeatureFlag/FeatureFlag.SDK/FeatureFlag.cs
+++ FeatureFlag/FeatureFlag.SDK/FeatureFlag.cs
@@ -4,36 +4,6 @@ namespace FeatureFlag.SDK
 {
     public class FeatureFlags
     {
-        MemoryCache cache = MemoryCache.Default;
-        public FeatureFlags()
-        {
-                
-        }
-        public bool IsFeatureFlagEnabled(FeatureFlagModel featureFlagModel)
-        {
-            return (bool)cache[featureFlagModel.FlagName];
-        }
-
-        public void AddFeatureFlag(FeatureFlagModel featureFlagModel)
-        {
-
-            string key = "myKey";
-            cache.Add(new CacheItem(key) { Value = true}, new CacheItemPolicy() { });
-
-            //if (value == null)
-            //{
-            //    value = true;
-            //    cache.Set(key, value, DateTimeOffset.Now.AddMinutes(5));
-            //}
-
-        }
-        public void UpdateFeatureFlag(FeatureFlagModel featureFlagModel)
-        {
-
-        }
-        public void DeleteFeatureFlag(FeatureFlagModel featureFlagModel)
-        {
-
-        }
+       
     }
 }
---
+++ FeatureFlag/FeatureFlag.SDK/FlagDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeatureFlag.SDK
+{
+    public class FlagDAL
+    {
+        public void InsertFeatureFlag(AddFeatureFlagModel addFeatureFlagModel)
+        {
+
+        }
+    }
+}
